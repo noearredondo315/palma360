ebe99c8f194f59b1701b3574b6b39f73d891c6bd
Noe Arredondo <noearredondo3105@gmail.com>
2025-05-23 02:49:13 -0600

    Full test

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ def main():
     # print (obras_lista)
     
     # Limitar a 3 obras para la prueba
-    obras_prueba = obras_lista[:40] if len(obras_lista) > 40 else obras_lista
-    # obras_prueba = obras_lista
+    # obras_prueba = obras_lista[:40] if len(obras_lista) > 40 else obras_lista
+    obras_prueba = obras_lista
     # 2. Inicializar gestor de facturas
     invoice_manager = InvoiceManager(session, base_data_path=APP_DATA_DIR)
     
